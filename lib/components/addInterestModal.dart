2ccde178e9399330fb30f34217302bab88124de5
GungIndi <gungindikusumaputra@gmail.com>
2024-06-30 21:25:04 +0800

    add mediaQuery

--- lib/components/addInterestModal.dart
+++ lib/components/addInterestModal.dart
@@ -28,49 +28,56 @@ class _AddInterestModalState extends State<AddInterestModal> {
 
   @override
   Widget build(BuildContext context) {
-    return CustomModalBottomSheet(
-      height: 300,
-      content: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Tambah Bunga',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.w600
-              ),
+    return Padding(
+      padding: EdgeInsets.only(
+        bottom: MediaQuery.of(context).viewInsets.bottom,
+      ),
+      child: SingleChildScrollView(
+        child: CustomModalBottomSheet(
+          height: 300,
+          content: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Tambah Bunga',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600
+                  ),
+                ),
+              ],
             ),
+            SizedBox(height: 25),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  'Persen Bunga',
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w600
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            CustomInputField(
+              hintText: 'Masukan Persenan Bunga',
+              controller: persenController,
+            ),
+            SizedBox(height: 25),
+            CustomButton(
+              text: 'Tambah',
+              backgroundColor: Colors.blue,
+              textColor: Colors.white,
+              onPressed: () {
+                addInterest();
+              }
+            )
           ],
         ),
-        SizedBox(height: 25),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Text(
-              'Persen Bunga',
-              style: TextStyle(
-                fontSize: 15,
-                fontWeight: FontWeight.w600
-              ),
-            ),
-          ],
-        ),
-        SizedBox(height: 10),
-        CustomInputField(
-          hintText: 'Masukan Persenan Bunga',
-          controller: persenController,
-        ),
-        SizedBox(height: 25),
-        CustomButton(
-          text: 'Tambah',
-          backgroundColor: Colors.blue,
-          textColor: Colors.white,
-          onPressed: () {
-            addInterest();
-          }
-        )
-      ],
+      ),
     );
   }
 }
